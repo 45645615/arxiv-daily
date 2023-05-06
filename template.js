9a4b372d6cf958cae3adbc1f448ccc426de1ab6a
David Yung <ldavidf97@gmail.com>
2023-05-06 02:46:12 +0100

    changes to prompt

--- template.js
+++ template.js
@@ -13,18 +13,15 @@ const ARXIV_TERM = 1;
 // const CHANNEL = '#daily-dose-of-arxiv'
 const CHANNEL = '#arxiv-debug'
 
-const SELECTION_PREFIX = `You are an expert in deep learning. Your PhD student studies on applying Deep Learning onto Computer Vision, Medical Imaging, and Multimodal learning.\
-     Below is a list of paper title from arxiv.\
-     Select ${MAX_PAPER_COUNT} most interesting paper that you think will inspire your student in thier research. Please provide the numbers corresponding to the papers you selected, separated by commas. For example: \"Title index, Title index, Title index, Title index, Title index, Title index, Title index, Title index, Title index, Title index\"`;
+const SELECTION_PREFIX = `You are an expert in deep learning. Your PhD student is working on applying Deep Learning to Computer Vision, Medical Imaging, and Multimodal learning. Below is a list of paper titles from arXiv.
+     Select the ${MAX_PAPER_COUNT} most interesting papers that you think will inspire your student in their research. Please provide the numbers corresponding to the papers you selected, separated by commas. For example: \"Title index, Title index, Title index, Title index, Title index, Title index, Title index, Title index, Title index, Title index\"`;
 
-const PROMPT_PREFIX = `You are an expert in deep learning. Summarize the following 
- paper for a PhD student working on medical imaging. 
- Provide a sectence no more than 10 words discribing the main purpose of the paper in TLDR. 
- Provide 3-4 key bullet points with each no more than 20 words in Summary, also provide relevance score in the format of {x/10}, a brief reason for 
- the score, and 1-2 specific applications related to the audience's 
- research interest (e.g., diagnostics, treatment planning, medical imaging). 
- Keep it simple and concise, you can replace words with vocabulary easier to 
- understand when necessary. For example: 
+const PROMPT_PREFIX = `You are an expert in deep learning. Summarize the following paper for a PhD student working on medical imaging.
+ Provide a sentence, no more than 10 words, describing the main purpose of the paper in TLDR. 
+ Provide 3-4 key bullet points, with each no more than 20 words, in Summary. 
+ Also, provide a relevance score in the format of {x/10}, a brief reason for the score, and 1-2 specific applications related to the PhD student's research interest (e.g., diagnostics, treatment planning, medical imaging).
+ Keep it simple and concise. Replace words with easier-to-understand vocabulary when necessary.
+ For example: 
 
  TLDR:
 - Enhanced skin lesion segmentation using edge-body fusion.
@@ -41,34 +38,30 @@ Applications:
 - Improved skin cancer diagnosis in medical imaging.
 - Enhanced treatment planning based on precise lesion boundaries.`;
 
-const WEEKENDSELECTION_PREFIX = `You are an expert in deep learning.\
-     Below is a list of paper title from topics of Mathematics, Statistics and Economics. \
-     Select 5 most interesting paper that you think will inspire your student in thier research. Please provide the numbers corresponding to the papers you selected, separated by commas. For example: \"Title index, Title index, Title index, Title index, Title index, Title index, Title index, Title index, Title index, Title index\"`;
+const WEEKENDSELECTION_PREFIX = `You are an expert in deep learning. Below is a list of paper titles from the topics of Mathematics, Statistics, Economics, and Q-Fin.
+Select the 10 most interesting papers that you think will inspire your student in their research. Please provide the numbers corresponding to the papers you selected, separated by commas. For example: \"Title index, Title index, Title index, Title index, Title index, Title index, Title index, Title index, Title index, Title index"`;
 
-const WEEKENDPROMPT_PREFIX = `You are an expert in deep learning and Mathematics. \
-Your audience is a PhD student working on deep learning.\
- Please provide a sectence no more than 10 words discribing the main purpose of the paper in TLDR.
- Please summarize the following Mathematics paper in a way that is accessible for someone \
- without a Mathematics PhD by providing 3-7 key bullet points in Summary, each no more than 20 words, you can replace words with vocabulary easier to \
- understand when necessary.\
-  Suggest possible implementations of this paper for the student's research\
-  interests in deep learning in Applications. For example:
+const WEEKENDPROMPT_PREFIX = `You are an expert in deep learning and Mathematics. Your audience is a PhD student working on deep learning. 
+ Please provide a TLDR sentence (no more than 10 words) describing the main purpose of the paper in TLDR.
+ Please summarize the following Mathematics paper in a way that is accessible for someone with an undergraduate Mathematics background by providing 3-4 key bullet points in the Summary section. 
+ Each bullet point should be no more than 20 words, and you may replace words with easier vocabulary when necessary.
+ Suggest possible implementations of this paper for the student's research interests in deep learning in the Applications section. For example:
 
   TLDR:
-- This paper proposes a method for testing independence between graphs in social network analysis and neuroscience.
+- Analyzing hyperbolic surfaces' moduli spaces and mapping class group cohomology.
 
   Summary:
-- The paper studies independence testing for inhomogeneous Erdős-Rényi random graphs on the same vertex set.
-- The authors formulate a notion of pairwise correlations between the edges of these graphs and derive a necessary condition for their detectability.
-- The authors show that there can be a statistical vs. computational tradeoff for detecting correlations in these graphs.
-- The paper proposes an asymptotically valid and consistent test procedure for correlation testing when the graphs are sampled from a latent space model (graphon).
+- Investigates moduli space Mg of hyperbolic surfaces.
+- Focuses on surfaces with "enough" short geodesics.
+- Discovers locus as classifying space of handlebody mapping class group.
+- Top weight cohomology of Mg maps injectively to cohomology of handlebody mapping class group.
 
 Applications:
-- The proposed method could be useful in social network analysis and neuroscience to test for independence between graphs.
-- Understanding the statistical vs. computational tradeoff could be relevant for developing more efficient algorithms for graph analysis.
-- The study of latent space models could have implications for modeling certain types of data in deep learning, such as generating synthetic data for training neural networks.`
+- Use findings to improve topological data analysis for deep learning architectures.
+- Enhance generative models by incorporating hyperbolic geometry and moduli spaces.
+- Develop novel optimization algorithms inspired by handlebody mapping class groups.`
 
-CATEGORIES_GROUPT_PREFIX = `You are an expert in deep learning. Below are titles of deep learning paper from arxiv, group them into categories based on their similarity, in format of {category name: paper number}, you can use different category name than example, avoid groups with only single element. For example:
+CATEGORIES_GROUPT_PREFIX = `You are an expert in deep learning. Below are the titles of deep learning papers from the arXiv. Group them into categories based on their similarity, using the format {category name: paper number}. You can use different category names than the example. If possible, avoid groups with only a single element. For example:
 {
   "Medical Imaging": [Title indices],
   "Category Name": [Title indices],
